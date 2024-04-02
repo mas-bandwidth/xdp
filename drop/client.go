b557a4bf6d103bdcd053900bd2c9513361880bb1
Glenn Fiedler <glenn@networknext.com>
2024-04-02 19:35:03 -0400

    drop

---
+++ drop/client.go
@@ -0,0 +1,64 @@
+
+package main
+
+import (
+	"net"
+	"time"
+	"fmt"
+	"strconv"
+	"context"
+)
+
+func ParseAddress(input string) net.UDPAddr {
+	address := net.UDPAddr{}
+	ip_string, port_string, err := net.SplitHostPort(input)
+	if err != nil {
+		address.IP = net.ParseIP(input)
+		address.Port = 0
+		return address
+	}
+	address.IP = net.ParseIP(ip_string)
+	address.Port, _ = strconv.Atoi(port_string)
+	return address
+}
+
+func main() {
+
+	// todo: read address from command line
+	address := ParseAddress("192.168.1.40:40000")
+	if address.Port == 0 {
+		address.Port = 40000
+	}
+
+	lc := net.ListenConfig{}
+
+	lp, err := lc.ListenPacket(context.Background(), "udp", "0.0.0.0:30000")
+	if err != nil {
+		panic(fmt.Sprintf("could not bind socket: %v", err))
+	}
+
+	conn := lp.(*net.UDPConn)
+
+	for i := 0; i < 10; i++ {
+
+		packet := make([]byte, 256 )
+
+		fmt.Printf("sent %d byte packet to %s\n", len(packet), address.String())
+
+		conn.WriteToUDP(packet, &address)
+
+		time.Sleep(time.Millisecond*100)
+	}
+
+	for {
+
+		buffer := make([]byte, 1384)
+
+		size, from, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			continue
+		}
+
+		fmt.Printf("received %d byte packet from %s\n", size, from.String())
+	}
+}
---
+++ drop/drop_xdp.c
@@ -0,0 +1,88 @@
+/*
+    UDP drop XDP program
+
+    Reflects IPv4 UDP packets sent to port 40000 back to sender.
+
+    USAGE:
+
+        clang -Ilibbpf/src -g -O2 -target bpf -c drop_xdp.c -o drop_xdp.o
+        sudo cat /sys/kernel/debug/tracing/trace_pipe
+*/
+
+#include <linux/in.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <linux/if_vlan.h>
+#include <linux/ip.h>
+#include <linux/ipv6.h>
+#include <linux/udp.h>
+#include <linux/bpf.h>
+#include <linux/string.h>
+#include <bpf/bpf_helpers.h>
+
+#if defined(__BYTE_ORDER__) && defined(__ORDER_LITTLE_ENDIAN__) && \
+    __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#define bpf_ntohs(x)        __builtin_bswap16(x)
+#define bpf_htons(x)        __builtin_bswap16(x)
+#elif defined(__BYTE_ORDER__) && defined(__ORDER_BIG_ENDIAN__) && \
+    __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#define bpf_ntohs(x)        (x)
+#define bpf_htons(x)        (x)
+#else
+# error "Endianness detection needs to be set up for your compiler?!"
+#endif
+
+#define DEBUG 1
+
+#if DEBUG
+#define debug_printf bpf_printk
+#else // #if DEBUG
+#define debug_printf(...) do { } while (0)
+#endif // #if DEBUG
+
+SEC("drop_xdp") int drop_xdp_filter( struct xdp_md *ctx ) 
+{ 
+    void * data = (void*) (long) ctx->data; 
+
+    void * data_end = (void*) (long) ctx->data_end; 
+    struct ethhdr * eth = data;
+
+    if ( (void*)eth + sizeof(struct ethhdr) < data_end )
+    {
+        if ( eth->h_proto == __constant_htons(ETH_P_IP) ) // IPV4
+        {
+            struct iphdr * ip = data + sizeof(struct ethhdr);
+
+            if ( (void*)ip + sizeof(struct iphdr) < data_end )
+            {
+                if ( ip->protocol == IPPROTO_UDP ) // UDP
+                {
+                    struct udphdr * udp = (void*) ip + sizeof(struct iphdr);
+
+                    if ( (void*)udp + sizeof(struct udphdr) <= data_end )
+                    {
+                        if ( udp->dest == __constant_htons(40000) )
+                        {
+                            void * payload = (void*) udp + sizeof(struct udphdr);
+                            int payload_bytes = data_end - payload;
+
+                            if ( payload_bytes < 16 )
+                            {
+                                debug_printf( "packet is too small to be valid" );
+                                return XDP_DROP;
+                            }
+
+                            // todo: check pattern                        
+                            
+                            return XDP_PASS;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
