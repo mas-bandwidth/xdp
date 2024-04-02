07c4d13db5eaeab58031cd2497294fd5d8b61485
Glenn Fiedler <glenn@networknext.com>
2024-04-02 19:49:43 -0400

    wip on drop

--- drop/drop_xdp.c
+++ drop/drop_xdp.c
@@ -64,15 +64,56 @@ SEC("drop_xdp") int drop_xdp_filter( struct xdp_md *ctx )
                         if ( udp->dest == __constant_htons(40000) )
                         {
                             void * payload = (void*) udp + sizeof(struct udphdr);
+
                             int payload_bytes = data_end - payload;
 
-                            if ( payload_bytes < 16 )
+                            // Drop packets that are too small to be valid
+
+                            if ( (void*)packet_data + 16 > data_end )
                             {
-                                debug_printf( "packet is too small to be valid" );
+                                debug_printf( "packet is too small" );
                                 return XDP_DROP;
                             }
 
-                            // todo: check pattern                        
+                            // Drop packets that are too large to be valid
+
+                            int packet_bytes = data_end - (void*)udp - sizeof(struct udphdr);
+
+                            if ( packet_bytes > 1400 )
+                            {
+                                debug_printf( "packet is too large" );
+                                return XDP_DROP;
+                            }
+
+                            // Drop UDP packet if it is a fragment
+
+                            if ( ( ip->frag_off & ~0x2000 ) != 0 )
+                            {
+                                debug_printf( "dropped udp fragment" );
+                                return XDP_DROP;
+                            }
+
+                            // Basic packet filter
+
+                            if ( packet_data[2] != ( 1 | ( ( 255 - packet_data[1] ) ^ 113 ) )                                               ||
+                                 packet_data[3] < 0x2A || packet_data[3] > 0x2D                                                             ||
+                                 packet_data[4] < 0xC8 || packet_data[4] > 0xE7                                                             ||
+                                 packet_data[5] < 0x05 || packet_data[5] > 0x44                                                             ||
+                                 packet_data[7] < 0x4E || packet_data[7] > 0x51                                                             ||
+                                 packet_data[8] < 0x60 || packet_data[8] > 0xDF                                                             ||
+                                 packet_data[9] < 0x64 || packet_data[9] > 0xE3                                                             ||
+                                 packet_data[10] != 0x07 && packet_data[10] != 0x4F                                                         ||
+                                 packet_data[11] != 0x25 && packet_data[11] != 0x53                                                         ||
+                                 packet_data[12] < 0x7C || packet_data[12] > 0x83                                                           ||
+                                 packet_data[13] < 0xAF || packet_data[13] > 0xB6                                                           ||
+                                 packet_data[14] < 0x21 || packet_data[14] > 0x60                                                           ||
+                                 packet_data[15] != 0x61 && packet_data[15] != 0x05 && packet_data[15] != 0x2B && packet_data[15] != 0x0D )
+                            {
+                                debug_printf( "packet filter dropped packet" );
+                                return XDP_DROP;
+                            }
+
+                            debug_printf( "packet filter passed" );
                             
                             return XDP_PASS;
                         }
