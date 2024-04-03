ed6bff2402fa252945e40cd76ea44212ce01b4c9
Glenn Fiedler <glenn@networknext.com>
2024-04-03 18:00:24 -0400

    more

--- crypto/crypto_module.c
+++ crypto/crypto_module.c
@@ -36,7 +36,6 @@ __bpf_kfunc int bpf_crypto_sha256( void * data, int data__sz, void * output, int
     SHASH_DESC_ON_STACK( shash, tfm );
     shash->tfm = sha256;
     crypto_shash_digest( shash, data, data__sz, output );
-    sha256_hash( data, data__sz, output );
     return 0;
 }
 
@@ -60,45 +59,6 @@ static int __init crypto_init( void )
     {
         pr_err( "can't create sha256 crypto hash algorithm\n" );
         return PTR_ERR( sha256 );
-    }
-
-    __u8 digest[32];
-    sha256_hash( "test", 4, digest );
-    if ( digest[0]  != 0x9f || 
-         digest[1]  != 0x86 ||
-         digest[2]  != 0xd0 ||
-         digest[3]  != 0x81 ||
-         digest[4]  != 0x88 ||
-         digest[5]  != 0x4c ||
-         digest[6]  != 0x7d ||
-         digest[7]  != 0x65 ||
-         digest[8]  != 0x9a ||
-         digest[9]  != 0x2f ||
-         digest[10] != 0xea ||
-         digest[11] != 0xa0 ||
-         digest[12] != 0xc5 ||
-         digest[13] != 0x5a ||
-         digest[14] != 0xd0 ||
-         digest[15] != 0x15 ||
-         digest[16] != 0xa3 ||
-         digest[17] != 0xbf ||
-         digest[18] != 0x4f ||
-         digest[19] != 0x1b ||
-         digest[20] != 0x2b ||
-         digest[21] != 0x0b ||
-         digest[22] != 0x82 ||
-         digest[23] != 0x2c ||
-         digest[24] != 0xd1 ||
-         digest[25] != 0x5d ||
-         digest[26] != 0x6c ||
-         digest[27] != 0x15 ||
-         digest[28] != 0xb0 ||
-         digest[29] != 0xf0 ||
-         digest[30] != 0x0a ||
-         digest[31] != 0x08 )
-    {
-        pr_err( "sha256 is broken\n" );
-        return -1;
     }
 
     int result = register_btf_kfunc_id_set( BPF_PROG_TYPE_XDP, &bpf_task_kfunc_set );
