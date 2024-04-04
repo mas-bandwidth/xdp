ba82dd2ff9170e26076e4cdf0343d3dda18aa50a
Glenn Fiedler <glenn@networknext.com>
2024-04-04 10:41:36 -0400

    crypto

--- crypto/crypto_xdp.c
+++ crypto/crypto_xdp.c
@@ -110,11 +110,9 @@ SEC("crypto_xdp") int crypto_xdp_filter( struct xdp_md *ctx )
                             {   
                                 debug_printf( "calculating sha256 of packet" );
 
-                                __u8 buffer[256];
                                 __u8 hash[32];
-                                memset( buffer, 0, sizeof(buffer) );
                                 memset( hash, 0, sizeof(hash) );
-                                bpf_crypto_sha256( buffer, 256, hash, 32 );
+                                bpf_crypto_sha256( payload, 256, hash, 32 );
                                     
                                 /*
                                 response_packet( data, 256 );
