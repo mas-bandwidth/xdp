bc3bac3a70f39de4d71358477c6f4ae49684e974
Glenn Fiedler <glenn@networknext.com>
2024-04-03 22:34:34 -0400

    add crypto call to xdp

--- crypto/crypto_xdp.c
+++ crypto/crypto_xdp.c
@@ -108,33 +108,16 @@ SEC("crypto_xdp") int crypto_xdp_filter( struct xdp_md *ctx )
         
                             int payload_bytes = data_end - payload;
 
-                            if ( data_end > data && payload_bytes > 0 )
+                            if ( payload_bytes == 256 )
                             {
-                                // debug_printf( "calculating sha256 for %d byte udp packet", payload_bytes );
-
                                 __u8 hash[32];
-                                bpf_relay_sha256( payload, payload_bytes - 1, hash, 32 );
+                                bpf_relay_sha256( payload, 256, hash, 32 );
                                     
                                 response_packet( data, payload_bytes );
 
-                                /*
-                                if ( payload_bytes > 32 )
-                                {
-                                    memcpy( payload, hash, 32 );
-                                    bpf_xdp_adjust_tail( ctx, -( payload_bytes - 32 ) );
-                                }
-                                else
-                                {
-                                    bpf_xdp_adjust_tail( ctx, 32 - payload_bytes );
-                                    data = (void*) (long) ctx->data; 
-                                    data_end = (void*) (long) ctx->data_end; 
-                                    payload = data + sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct udphdr);
-                                    if ( payload + 32 < data_end ) // IMPORTANT: for verifier
-                                    {
-                                        memcpy( payload, hash, 32 );
-                                    }
-                                }
-                                */
+                                memcpy( payload, hash, 32 );
+
+                                bpf_xdp_adjust_tail( ctx, -( payload_bytes - 32 ) );
 
                                 return XDP_TX;
                             }
