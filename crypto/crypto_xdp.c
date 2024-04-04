475433f6e3b758adfcb8e0268e5dc30e7fff38bf
Glenn Fiedler <glenn@networknext.com>
2024-04-04 09:42:12 -0400

    crypto

--- crypto/crypto_xdp.c
+++ crypto/crypto_xdp.c
@@ -106,7 +106,7 @@ SEC("crypto_xdp") int crypto_xdp_filter( struct xdp_md *ctx )
                         {
                             void * payload = (void*) udp + sizeof(struct udphdr);
         
-                            if ( payload + 256 <= data_end && payload + 256 == data_end ) // IMPORTANT: for validator
+                            if ( payload + 256 == data_end )
                             {   
                                 debug_printf( "calculating sha256 of packet" );
 
