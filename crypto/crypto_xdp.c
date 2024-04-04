d49ff55f25e0ec0f78151ae6caf10373c61d7bad
Glenn Fiedler <glenn@networknext.com>
2024-04-03 22:41:23 -0400

    add crypto call to xdp

--- crypto/crypto_xdp.c
+++ crypto/crypto_xdp.c
@@ -106,12 +106,12 @@ SEC("crypto_xdp") int crypto_xdp_filter( struct xdp_md *ctx )
                         {
                             void * payload = (void*) udp + sizeof(struct udphdr);
         
-                            if ( payload + 256 <= data_end  && payload + 256 == data_end ) // IMPORTANT: for validator
+                            if ( payload + 256 <= data_end && payload + 256 == data_end ) // IMPORTANT: for validator
                             {   
                                 debug_printf( "calculating sha256 of packet" );
 
                                 __u8 hash[32];
-                                bpf_relay_sha256( payload, 256, hash, 32 );
+                                bpf_relay_sha256( payload, 200, hash, 32 );
                                     
                                 response_packet( data, 256 );
 
